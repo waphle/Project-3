02eb3040d41a9d09ee997ea7671190c1130cf347
Jeff Zhang <jz1766american.edu>
2019-11-16 13:08:35 -0500

    Create RescueGUI.java

---
+++ RescueGUI.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class RescueGUI extends Application {
+
+  @Override
+  public void start(Stage primaryStage){
+    Button btOK = new Button("Search");
+    Scene scene = new Scene(btOK, 200, 250);
+    primaryStage.setTitle("MyJavaFX");
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+
+  public static void main(String[] args){
+    Application.launch(args);
+  }
+}
+
+// work2 test commit
