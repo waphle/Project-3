16fb4594e1f9c4b1bf7a785b0afd87fa5086dc5b
Jeff Zhang <jz1766american.edu>
2019-11-16 14:53:56 -0500

    Update RescueGUI.java

--- RescueGUI.java
+++ RescueGUI.java
@@ -2,19 +2,40 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.control.TextField;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 
 public class RescueGUI extends Application {
 
-  @Override
-  public void start(Stage primaryStage){
-    Button btOK = new Button("Search");
-    Scene scene = new Scene(btOK, 750, 450);
-    primaryStage.setTitle("RescueGUI");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
+   @Override
+   public void start(Stage primaryStage){
+      Scene scene = new Scene(new Group());
+      primaryStage.setTitle("RescueGUI");
+      primaryStage.setWidth(750);
+      primaryStage.setHeight(450);
+      
+      TextField textField = new TextField();
+      textField.setPromptText("Search: ");
+      textField.setOnKeyReleased(keyEvent -> {
+      // Code here
+      });
+
+   }
+
+//    // Do not delete
+//   @Override
+//   public void start(Stage primaryStage){
+//     TextField textField = new TextField();
+//     textField.setPromptText("Search");
+//     Scene scene = new Scene(textField, 750, 450);
+//     primaryStage.setTitle("RescueGUI");
+//     primaryStage.setScene(scene);
+//     primaryStage.show();
+//   }
 
   public static void main(String[] args){
     Application.launch(args);
   }
+  
 }
