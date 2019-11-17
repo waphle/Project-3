812398ac008659ded0e555560a92f8f8590f217a
Zhang, Jason <jzhang636@gmail.com>
2019-11-16 22:26:38 -0500

    Use a HashMap to hold the state-count data structure

--- RescueGUI.java
+++ RescueGUI.java
@@ -10,6 +10,8 @@ import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import java.util.Random;
+import java.util.HashMap; 
+import java.util.Map; 
 
 public class RescueGUI extends Application {
 
@@ -26,77 +28,8 @@ public class RescueGUI extends Application {
   final int TextField_Width = 800;
   final int TextField_Height = 40;
   
-  // Bar chart data
-  final int Num_States = 51;
-  int Counts[] = new int[Num_States];
-  String States[] = new String[Num_States];
-  
-  @Override
-  public void start(Stage primaryStage) {
-  
-    // Prepare test data
-    prepareStateNames();
-    prepareCountData();
-
-    // Layout the application GUI
-    // Side offsets for outter pane in the scene
-    FlowPane outterPane = new FlowPane();
-    outterPane.setPadding(new Insets(Side_Padding, Side_Padding, Side_Padding, Side_Padding));
-    outterPane.setHgap(Pane_H_Gap);
-    outterPane.setVgap(Pane_V_Gap);
-    
-    // Search button
-    StackPane btnPane = new StackPane();
-    Button btnSearch = new Button("Search");
-    btnSearch.setFont(Font.font("Arial", 18));
-    btnSearch.setPrefWidth(Button_Width);
-    btnPane.getChildren().add(btnSearch);
-    
-    // Search text field
-    TextField srchText = new TextField();
-    srchText.setPrefColumnCount(1);
-    srchText.setPromptText("Enter a key word to search in the ads");
-    srchText.setPrefWidth(TextField_Width);
-    srchText.setPrefHeight(TextField_Height);
-    srchText.setFont(Font.font("Arial", 16));
-    
-    // Search result bar chart
-    CategoryAxis xAxis = new CategoryAxis();
-    NumberAxis yAxis = new NumberAxis();
-    BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
-    barChart.setHorizontalGridLinesVisible(false);
-    barChart.setVerticalGridLinesVisible(false);
-    barChart.setLegendVisible(false);
-    barChart.setCategoryGap(2);
-    barChart.setBarGap(0);
-    barChart.setMinWidth(Scene_Width*0.90);
-    barChart.setMaxWidth(Scene_Width);
-    
-    xAxis.setLabel("State");
-    yAxis.setLabel("Match Count");
-    
-    XYChart.Series series = new XYChart.Series();
-    for (int i = 0; i < Num_States; i++) {
-       series.getData().add(new XYChart.Data(States[i], Counts[i]));
-    }
-    barChart.getData().addAll(series);
-
-    // Place nodes in the outter pane
-    outterPane.getChildren().addAll(barChart, srchText, btnPane);
-    
-    // Construct the scene
-    Scene scene = new Scene(outterPane, Scene_Width, Scene_Hight);
-    
-    // Stage
-    primaryStage.setTitle("Rescue");
-    primaryStage.setScene(scene);
-    primaryStage.setResizable(false);
-    primaryStage.show();
-  }
-
-  // Generate the states' abbreviation name codes
-  private void prepareStateNames() {
-    String[] stateAbbNames = {
+  // Search count data
+  final String[] stateAbbNames = {
       "AL",
       "AK",
       "AZ",
@@ -148,17 +81,81 @@ public class RescueGUI extends Application {
       "WV",
       "WI",
       "WY" };
-
-      for(int i = 0; i < Num_States; i++) {
-         States[i] = stateAbbNames[i];
-      }
-  }
+  final int Num_States = 51;
+  int Counts[] = new int[Num_States];
+  String States[] = new String[Num_States];
+  public static HashMap<String, Integer> State_Count_Map = new HashMap<String, Integer>();
   
+  @Override
+  public void start(Stage primaryStage) {
+  
+    // Prepare test data
+    prepareCountData();
+
+    // Layout the application GUI
+    // Side offsets for outter pane in the scene
+    FlowPane outterPane = new FlowPane();
+    outterPane.setPadding(new Insets(Side_Padding, Side_Padding, Side_Padding, Side_Padding));
+    outterPane.setHgap(Pane_H_Gap);
+    outterPane.setVgap(Pane_V_Gap);
+    
+    // Search button
+    StackPane btnPane = new StackPane();
+    Button btnSearch = new Button("Search");
+    btnSearch.setFont(Font.font("Arial", 18));
+    btnSearch.setPrefWidth(Button_Width);
+    btnPane.getChildren().add(btnSearch);
+    
+    // Search text field
+    TextField srchText = new TextField();
+    srchText.setPrefColumnCount(1);
+    srchText.setPromptText("Enter a key word to search in the ads");
+    srchText.setPrefWidth(TextField_Width);
+    srchText.setPrefHeight(TextField_Height);
+    srchText.setFont(Font.font("Arial", 16));
+    
+    // Search result bar chart
+    CategoryAxis xAxis = new CategoryAxis();
+    NumberAxis yAxis = new NumberAxis();
+    BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
+    barChart.setHorizontalGridLinesVisible(false);
+    barChart.setVerticalGridLinesVisible(false);
+    barChart.setLegendVisible(false);
+    barChart.setCategoryGap(2);
+    barChart.setBarGap(0);
+    barChart.setMinWidth(Scene_Width*0.90);
+    barChart.setMaxWidth(Scene_Width);
+    
+    xAxis.setLabel("State");
+    yAxis.setLabel("Match Count");
+    
+    XYChart.Series series = new XYChart.Series();
+//     for (int i = 0; i < Num_States; i++) {
+//        series.getData().add(new XYChart.Data(States[i], Counts[i]));
+//     }
+    State_Count_Map.forEach((k, v) -> { series.getData().add(new XYChart.Data(k, v)); });
+    barChart.getData().addAll(series);
+
+    // Place nodes in the outter pane
+    outterPane.getChildren().addAll(barChart, srchText, btnPane);
+    
+    // Construct the scene
+    Scene scene = new Scene(outterPane, Scene_Width, Scene_Hight);
+    
+    // Stage
+    primaryStage.setTitle("Rescue");
+    primaryStage.setScene(scene);
+    primaryStage.setResizable(false);
+    primaryStage.show();
+  }
+
   // Generate dummy random count data
   private void prepareCountData() {
       Random random = new Random();
       for(int i = 0; i < Num_States; i++) {
+         States[i] = stateAbbNames[i];
          Counts[i] = random.nextInt(100);
+         State_Count_Map.put(stateAbbNames[i], Counts[i]);
       }
   }
 }
