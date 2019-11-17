96289f9c38d9aea137d376e389748072c2875714
Jeff Zhang <jz1766american.edu>
2019-11-17 17:39:43 -0500

    Update RescueGUI.java

--- RescueGUI.java
+++ RescueGUI.java
@@ -1,64 +1,14 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
-import javafx.scene.control.TextField;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-
-
-public class RescueGUI extends Application {
-
-   @Override
-   public void start(Stage primaryStage){
-      Scene scene = new Scene(new Group());
-      primaryStage.setTitle("RescueGUI");
-      primaryStage.setWidth(750);
-      primaryStage.setHeight(450);
-      primaryStage.setScene(scene);
-      primaryStage.show();
-   }
-   
-   public void start(Scene primaryScene){
-      Scene scene = new Scene(new Group());  
-      TextField textField = new TextField();
-      textField.setPromptText("Search: ");
-      Scene scene2 = new Scene(textField, 200, 100);
-      textField.setOnKeyReleased(keyEvent -> {
-      // Code here
-      });
-   }
-   
-  public static void main(String[] args){
-    Application.launch(args);
-  }
-  
-}
-   
-//    // Do not delete
-//       Stage stage = new Stage();
-//       stage.setScene(scene);
-//       stage.setTitle("Search: ");
-//       TextField textField = new TextField();
-//       textField.setPromptText("Search: ");
-//       Scene scene2 = new Scene(textField, 200, 100);
-//       textField.setOnKeyReleased(keyEvent -> {
-//       // Code here
-//       });
-//          
-//       stage.setScene(scene);
-//       stage.show();
-//    }
-
-      
-
-//    // Do not delete
-//   @Override
-//   public void start(Stage primaryStage){
-//     TextField textField = new TextField();
-//     textField.setPromptText("Search");
-//     Scene scene = new Scene(textField, 750, 450);
-//     primaryStage.setTitle("RescueGUI");
-//     primaryStage.setScene(scene);
-//     primaryStage.show();
-//   }
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
+import javafx.geometry.Insets;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import java.util.Random;
+import java.util.HashMap; 
+import java.util.Map; 
